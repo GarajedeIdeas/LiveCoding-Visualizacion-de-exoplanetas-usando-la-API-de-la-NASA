c14ac1c4625c53c5605e87ea2b730d7619074bfe
Dani Vicario <danigv.email@gmail.com>
2022-02-21 17:08:16 +0100

    first commit

---
+++ nasaExoplanets.js
@@ -0,0 +1,39 @@
+// Dani Vicario - nasaExoplanets experiment (threejs) - Mon Feb 21 14:13:52 CET 2022
+let camera;
+let scene;
+let renderer;
+let geometry;
+let material;
+let mesh;
+
+function init() {
+    camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10);
+    camera.position.z = 1;
+
+    scene = new THREE.Scene();
+
+    geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+    material = new THREE.MeshNormalMaterial();
+
+    mesh = new THREE.Mesh(geometry, material);
+    scene.background = new THREE.Color("black");
+    scene.add(mesh);
+
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.gammaFactor = 2.2;
+    renderer.gammaOutput = true;
+    document.body.appendChild(renderer.domElement);
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    mesh.rotation.x += 0.01;
+    mesh.rotation.y += 0.02;
+
+    renderer.render(scene, camera);
+}
+
+init();
+animate();
---
+++ readme.md
@@ -0,0 +1,26 @@
+## Webinar
+
+[AQUÍ](https://www.youtube.com/watch?v=4_sxETpcyKs)
+
+# Exoplanetas & NASA API
+
+### Por Dani Vicario
+
+Sígueme en Github
+https://github.com/danivicario
+
+Mis fotos de flores
+https://www.flickr.com/photos/danivicario
+
+Obras generativas en Behance
+https://www.behance.net/danivicario
+
+Enlace a la presentación: https://docs.google.com/presentation/d/1oP7hE90Zdw-I2dfNmA0U-K0SxbbZByoS4WbBOFTU8vc/edit?usp=sharing
+
+Para instalar el proyecto, ejecuta `npm install` en la carpeta raiz del proyecto y luego `npm start`
+
+Puedes visitar los archivos del proyecto en `http://127.0.0.1:8080/`
+
+http://127.0.0.1:8080/nasaExoplanets.html
+
+¡Muchas gracias!
