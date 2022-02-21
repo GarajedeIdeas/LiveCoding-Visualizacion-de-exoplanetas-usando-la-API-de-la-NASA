6693148134c9175501832ba3d37bd591847b0c35
Dani Vicario <danigv.email@gmail.com>
2022-02-21 17:36:12 +0100

    added prettier

--- nasaExoplanets.js
+++ nasaExoplanets.js
@@ -7,32 +7,34 @@ let material;
 let mesh;
 
 function init() {
-    camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10);
-    camera.position.z = 1;
+  camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10);
+  camera.position.z = 1;
 
-    scene = new THREE.Scene();
+  scene = new THREE.Scene();
 
-    geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
-    material = new THREE.MeshNormalMaterial();
+  geometry = new THREE.SphereGeometry(2, 3, 3);
+  material = new THREE.MeshNormalMaterial();
 
-    mesh = new THREE.Mesh(geometry, material);
-    scene.background = new THREE.Color("black");
-    scene.add(mesh);
+  mesh = new THREE.Mesh(geometry, material);
+  scene.background = new THREE.Color("black");
+  scene.add(mesh);
 
-    renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.gammaFactor = 2.2;
-    renderer.gammaOutput = true;
-    document.body.appendChild(renderer.domElement);
+  renderer = new THREE.WebGLRenderer({
+    antialias: true
+  });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.gammaFactor = 2.2;
+  renderer.gammaOutput = true;
+  document.body.appendChild(renderer.domElement);
 }
 
 function animate() {
-    requestAnimationFrame(animate);
+  requestAnimationFrame(animate);
 
-    mesh.rotation.x += 0.01;
-    mesh.rotation.y += 0.02;
+  mesh.rotation.x += 0.01;
+  mesh.rotation.y += 0.02;
 
-    renderer.render(scene, camera);
+  renderer.render(scene, camera);
 }
 
 init();
